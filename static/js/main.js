cc08f87ad98a5b0dc6bb4faab4a10abc3c4e88a6
gulnazik <49994456+gulnazik@users.noreply.github.com>
2019-04-26 21:32:17 -0400

    Files for website-resume

---
+++ static/js/main.js
@@ -0,0 +1,30 @@
+;$('#myTab a').click(function (e) {
+  e.preventDefault()
+  $(this).tab('show')
+});
+
+/*$('#portfolio a').click(function (e) {
+  e.preventDefault()
+  $(this).tab('show')
+});
+
+$('#resume a').click(function (e) {
+  e.preventDefault()
+  $(this).tab('show')
+});
+
+
+$('#education a').click(function (e) {
+  e.preventDefault()
+  $(this).tab('show')
+});
+
+$('#experience a').click(function (e) {
+  e.preventDefault()
+  $(this).tab('show')
+});
+
+$('#contacts a').click(function (e) {
+  e.preventDefault()
+  $(this).tab('show')
+});*/
